6c21f4e1a6ad9396cff6b71cb32de28734153f73
Rajesh <technocratmillenium@gmail.com>
2019-01-29 12:07:28 +0530

    Initial commit

---
+++ PJKTech.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJKTech.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started console app");
+        }
+    }
+}
